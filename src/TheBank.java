f17dbb52fc150abbd0879541561cca9148ca5363
Amr khalid Mahfouz Mohammed <162550786+Amr-Khalid-Mahfouz@users.noreply.github.com>
2025-05-10 20:02:26 +0300

    Update TheBank.java

--- src/TheBank.java
+++ src/TheBank.java
@@ -1,9 +1,10 @@
+import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 import java.util.Random;
 
-public class TheBank {
+public class TheBank implements Serializable{
     private List<BankAccount> bankAccounts;
 
     public TheBank() {
@@ -37,4 +38,5 @@ public class TheBank {
             account.setOTP(otp);
         }
     }
+
 }
